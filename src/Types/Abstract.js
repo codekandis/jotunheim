022868421fa05e4cad442bcde6467de7e5f88864
Christian Ramelow <info@codekandis.net>
2023-02-25 23:51:29 +0100

    Adds the existing codebase.

---
+++ src/Types/Abstract.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Represents the base class of any instantiable class.
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+export class Abstract extends EventTarget
+{
+	/**
+	 * Constructor method.
+	 */
+	constructor()
+	{
+		super();
+	}
+}
