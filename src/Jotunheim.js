b5725540ef6ddf4d4fd845b4a0e6c229de418021
Christian Ramelow <info@codekandis.net>
2023-03-02 09:27:17 +0100

    Adds the method `KeyboardEvent.prototype.isModified()`.

--- src/Jotunheim.js
+++ src/Jotunheim.js
@@ -2,6 +2,7 @@
 
 import './Dom/Document.js';
 import './Dom/HtmlElement.js';
+import './Dom/KeyboardEvent.js';
 import './Dom/NodeList.js';
 import './Net/FileList.js';
 import './Net/Http/Headers/Headers.js';
