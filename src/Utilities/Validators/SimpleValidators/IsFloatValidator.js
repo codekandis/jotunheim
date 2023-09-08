0396d4ae63b877fee94bd2de5b71827683d3174d
Christian Ramelow <info@codekandis.net>
2023-09-08 13:00:06 +0200

    Adds default validators.

---
+++ src/Utilities/Validators/SimpleValidators/IsFloatValidator.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import { AbstractValidator } from '../AbstractValidator.js';
+
+/**
+ * Represents a validator validating if a value is a float.
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+export class IsFloatValidator extends AbstractValidator
+{
+	/**
+	 * @inheritdoc
+	 */
+	get constraint()
+	{
+		return 'float';
+	}
+
+	/**
+	 * @inheritdoc
+	 * Validates if a specific value is a float.
+	 */
+	validate( value )
+	{
+		const isValid = 'number' === typeof value;
+		this.__dispatchValidationEvent( isValid, this.constraint, value );
+
+		return isValid;
+	}
+}
