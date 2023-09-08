3d7502dd2682fdd781a8ac4870c9122b652c52f5
Christian Ramelow <info@codekandis.net>
2023-09-08 12:59:44 +0200

    Adds the `ValidatorChain`.

---
+++ src/Utilities/Validators/ValidatorChain.js
@@ -0,0 +1,218 @@
+'use strict';
+
+import { Collection } from '../../Collections/Collection.js';
+import { Abstract } from '../../Types/Abstract.js';
+import { EventManager } from '../../Types/EventManager.js';
+import { InvalidTypeException } from '../../Types/InvalidTypeException.js';
+import { AbstractValidator } from './AbstractValidator.js';
+import { ValidationFailedEvent } from './ValidationFailedEvent.js';
+import { ValidationFailedEventArguments } from './ValidationFailedEventArguments.js';
+import { ValidationsFailedEvent } from './ValidationsFailedEvent.js';
+import { ValidationsFailedEventArguments } from './ValidationsFailedEventArguments.js';
+import { ValidationsSucceededEvent } from './ValidationsSucceededEvent.js';
+import { ValidationSucceededEvent } from './ValidationSucceededEvent.js';
+
+/**
+ * Represents the handler of any validator chain validator iteration.
+ * @callback ValidatorChain_ValidatorIterationHandler
+ * @param {AbstractValidator} validator The currently iterated validator.
+ * @param {Number} validatorIndex The index of the currently iterated validator.
+ */
+
+/**
+ * Represents a collection of chained validators.
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+export class ValidatorChain extends Abstract
+{
+	/**
+	 * Stores the validators of the validator chain.
+	 * @type {AbstractValidator[]}
+	 */
+	#_validators = [];
+
+	/**
+	 * Stores the error messages of the validation.
+	 * @type {Collection<String>}
+	 */
+	#_errorMessages = new Collection();
+
+	/**
+	 * Constructor method.
+	 * @param {...AbstractValidator} validators The initital validators of the validator chain.
+	 */
+	constructor( ...validators )
+	{
+		super();
+
+		this.add( ...validators );
+	}
+
+	/**
+	 * Gets the validation failed event.
+	 * @type {Function}
+	 */
+	get validationFailedEvent()
+	{
+		return new EventManager( ValidationFailedEvent, this );
+	}
+
+	/**
+	 * Gets the validation succeeded event.
+	 * @type {Function}
+	 */
+	get validationSucceededEvent()
+	{
+		return new EventManager( ValidationSucceededEvent, this );
+	}
+
+	/**
+	 * Gets the validations failed event.
+	 * @type {Function}
+	 */
+	get validationsFailedEvent()
+	{
+		return new EventManager( ValidationsFailedEvent, this );
+	}
+
+	/**
+	 * Gets the validations succeeded event.
+	 * @type {Function}
+	 */
+	get validationsSucceededEvent()
+	{
+		return new EventManager( ValidationsSucceededEvent, this );
+	}
+
+	/**
+	 * Gets the iterator to iterate over any validator chain's validator.
+	 * @returns {IterableIterator<*>} The iterator to iterate over any validator chain's validator.
+	 */
+	* #validators()
+	{
+		const validators = this.#_validators;
+		for ( const validator of validators )
+		{
+			yield validator;
+		}
+	}
+
+	/**
+	 * Gets the iterator to iterate over any validator chain's item.
+	 * @returns {IterableIterator<*>} The iterator to iterate over any validator chain's item.
+	 */
+	[ Symbol.iterator ]()
+	{
+		return this.#validators();
+	}
+
+	/**
+	 * Static constructor method.
+	 * @param {...AbstractValidator[]} arrays The arrays to create the validator chain from.
+	 * @returns {ValidatorChain} The created validator chain.
+	 * @throws {InvalidTypeException} The type of at least one of the passed arrays is not `array`.
+	 * @constructor
+	 */
+	static fromArray( ...arrays )
+	{
+		arrays.forEach(
+			( fetchedArray ) =>
+			{
+				if ( false === Array.isArray( fetchedArray ) )
+				{
+					throw InvalidTypeException.with_type( 'array', typeof fetchedArray );
+				}
+			}
+		);
+
+		const validatorChain = new ValidatorChain();
+		arrays.forEach(
+			( fetchedArray ) =>
+			{
+				validatorChain.add( ...fetchedArray );
+			}
+		);
+
+		return validatorChain;
+	}
+
+	/**
+	 * Invokes a specific iteration handler on every validator chain's validator.
+	 * @param {ValidatorChain_ValidatorIterationHandler} iterationHandler The iteration handler to invoke.
+	 */
+	forEach( iterationHandler )
+	{
+		this.#_validators.forEach( iterationHandler );
+	}
+
+	/**
+	 * Adds a variadic amount of validators to the validator chain.
+	 * @param {...AbstractValidator} validators The validators to add.
+	 */
+	add( ...validators )
+	{
+		this.#_validators.add( ...validators );
+	}
+
+	/**
+	 * Validates a value.
+	 * @param {*} value The value to validate.
+	 * @returns {Boolean} True if the value is valid, otherwise false.
+	 */
+	validate( value )
+	{
+		this.#_errorMessages.clear();
+
+		let isValid = true;
+		this.#_validators.forEach(
+			( fetchedValidator ) =>
+			{
+				fetchedValidator.validationFailedEvent( this.#validator_validationFailed );
+				fetchedValidator.validationSucceededEvent( this.#validator_validationSucceeded );
+
+				isValid = fetchedValidator.validate( value ) && isValid;
+
+				fetchedValidator.validationSucceededEvent.remove( this.#validator_validationSucceeded );
+				fetchedValidator.validationFailedEvent.remove( this.#validator_validationFailed );
+			}
+		);
+
+		if ( false === isValid )
+		{
+			const eventArguments = new ValidationsFailedEventArguments(
+				new Collection( ...this.#_errorMessages )
+			);
+
+			this.validationsFailedEvent.dispatch( eventArguments );
+		}
+		else
+		{
+			this.validationsSucceededEvent.dispatch();
+		}
+
+		return isValid;
+	}
+
+	/**
+	 * Handles the validation failed event of every validator.
+	 * @param {ValidationFailedEvent} event
+	 * @param {ValidationFailedEventArguments} event.detail.eventArguments
+	 */
+	#validator_validationFailed = ( event ) =>
+	{
+		this.#_errorMessages.add( event.detail.eventArguments.errorMessage );
+
+		const eventArguments = new ValidationFailedEventArguments( event.detail.eventArguments.errorMessage );
+		this.validationFailedEvent.dispatch( eventArguments );
+	};
+
+	/**
+	 * Handles the validation succeeded event of every validator.
+	 * @param {ValidationsSucceededEvent} event
+	 * @param {EventArguments} event.detail.eventArguments
+	 */
+	#validator_validationSucceeded = ( event ) =>
+	{
+		this.validationSucceededEvent.dispatch();
+	};
+}
