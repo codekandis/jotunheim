b425727adf940989058e6503dba73ffa03856c21
Christian Ramelow <info@codekandis.net>
2023-09-13 09:02:09 +0200

    Adds the `IsObjectValidator`.

---
+++ src/Utilities/Validators/SimpleValidators/IsObjectValidator.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import { AbstractValidator } from '../AbstractValidator.js';
+
+/**
+ * Represents a validator validating if a value is an object.
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+export class IsObjectValidator extends AbstractValidator
+{
+	/**
+	 * @inheritdoc
+	 */
+	get constraint()
+	{
+		return 'object';
+	}
+
+	/**
+	 * @inheritdoc
+	 * Validates if a specific value is an object.
+	 */
+	validate( value )
+	{
+		const isValid = 'object' === typeof value;
+		this.__dispatchValidationEvent( isValid, this.constraint, value );
+
+		return isValid;
+	}
+}
