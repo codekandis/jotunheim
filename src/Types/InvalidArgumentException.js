5703f8a56b3f7620c12bc3e30e9e6279fd111407
Christian Ramelow <info@codekandis.net>
2023-09-13 09:06:20 +0200

    Adds the `IsInvalidArgumentException`.

---
+++ src/Types/InvalidArgumentException.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import { InvalidValueException } from './InvalidValueException.js';
+
+/**
+ * Represents the exception if an argument is invalid.
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+export class InvalidArgumentException extends InvalidValueException
+{
+	/**
+	 * Static constructor method.
+	 * @param {String} name The name of the argument which is invalid.
+	 * @param {*} argument The argument which is invalid.
+	 * @returns {InvalidValueException}
+	 * @constructor
+	 */
+	static with_argument( name, argument )
+	{
+		return new this(
+			String.format`The argument \`${ 0 }\` with the value \`${ 1 }\` is invalid.`( argument )
+		);
+	}
+}
