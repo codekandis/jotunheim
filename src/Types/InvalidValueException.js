ef945343851c3fc5bc3416f30d42af0eeb1afe15
Christian Ramelow <info@codekandis.net>
2023-09-13 22:17:05 +0200

    Enhances the method `InvalidValueException.with_value()`.

--- src/Types/InvalidValueException.js
+++ src/Types/InvalidValueException.js
@@ -11,13 +11,19 @@ export class InvalidValueException extends LogicException
 	/**
 	 * Static constructor method.
 	 * @param {*} value The value which is invalid.
+	 * @param {?String} expected The expected value of the argument.
 	 * @returns {InvalidValueException}
 	 * @constructor
 	 */
-	static with_value( value )
+	static with_value( value, expected = null )
 	{
 		return new this(
-			String.format`The value \`${ 0 }\` is invalid.`( value )
+			String.format`The value \`${ 0 }\` is invalid.${ 1 }`(
+				value,
+				null === expected
+					? String.isEmpty
+					: String.format` \`${ 0 }\` expected.`( expected )
+			)
 		);
 	}
 }
