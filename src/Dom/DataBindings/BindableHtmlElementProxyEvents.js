c00eb575646508da332efebb8ed4482552d5b4af
Christian Ramelow <info@codekandis.net>
2024-11-13 21:20:51 +0100

    Refactors `BindableHtmlElementProxy` due to multiple bound events.

---
+++ src/Dom/DataBindings/BindableHtmlElementProxyEvents.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import { AbstractStatic } from '../../Types/AbstractStatic.js';
+
+/**
+ * Represents a flagable enumeration of bindable HTML element proxy events.
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+export class BindableHtmlElementProxyEvents extends AbstractStatic
+{
+	/**
+	 * Specifies that the binding must occur via all events of the HTML element.
+	 * @type {Number}
+	 */
+	static get ALL()
+	{
+		return BindableHtmlElementProxyEvents.INPUT | BindableHtmlElementProxyEvents.CHANGE;
+	}
+
+	/**
+	 * Specifies that the binding must occur via the `input` event of the HTML element.
+	 * @type {Number}
+	 */
+	static get INPUT()
+	{
+		return 1;
+	}
+
+	/**
+	 * Specifies that the binding must occur via the `change` event of the HTML element.
+	 * @type {Number}
+	 */
+	static get CHANGE()
+	{
+		return 2;
+	}
+}
