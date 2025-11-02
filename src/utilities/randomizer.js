f599defbcaacd48e4c0b853dc39f4cbe2e3a50c3
Christian Ramelow <info@codekandis.net>
2025-11-02 22:28:17 +0100

    Adds a randomizer.

---
+++ src/utilities/randomizer.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import { AbstractStatic } from '../types/abstract-static.js';
+
+/**
+ * Represents a randomizer.
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+export class Randomizer extends AbstractStatic
+{
+	/**
+	 * Gets a cryptographically secure random number `0 <= n < 1`.
+	 * @return {Number} The cryptographically secure random number `0 <= n < 1`.
+	 */
+	static randomNumber()
+	{
+		return crypto.getRandomValues(
+			new Uint32Array( 1 )
+		)[ 0 ] / 2 ** 32;
+	}
+}
